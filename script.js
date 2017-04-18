3d2adb4cb01483e6062322c813776d0fe799f5d9
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-04-17 18:49:02 -0700

    added tweet button to open new window

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ $(document).ready(function($) {
 	// generate new quote function on click
 	$("#getNewQuote").on('click', function(){
 
-		$.getJSON( "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(data) {
+		$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(data) {
     		$(".quote").html(data[0].content + " - " + data[0].title)
     	});
-
-	}) 
+	});
+	//tweet on click
+	$(".twitter-share").on('click', function(){
+		window.open("https://twitter.com/intent/tweet" + (".quote").html(data[0].content + " - " + data[0].title))
+	});
 
   });
