f2e6e90bb4e1373f0c7a03c52c8329a9decc8b02
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-04-17 18:05:55 -0700

    added quote api to generate upon clicking button

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-    $("#getNewQuote").on("click", function(){
-      $(".quote").html("Here is the message");
-    });
+$(document).ready(function($) {
+	// elimate storing data
+	$.ajaxSetup ({
+    	cache: false
+  	});
+	// generate new quote function on click
+	$("#getNewQuote").on('click', function(){
+
+		$.getJSON( "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(data) {
+    		$(".quote").html(data[0].content + " - " + data[0].title)
+    	});
+
+	}) 
+
   });
